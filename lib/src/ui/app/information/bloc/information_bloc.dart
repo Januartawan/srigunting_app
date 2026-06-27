13a0524ccf8e585bbf267f9770478fdf8c340ded
Januartawan <januarthawan@gmail.com>
2026-06-27 16:01:53 +0800

    Optimasi kode di information_bloc.dart, hapus import bloc, dan gunakan const pada list kosong

--- lib/src/ui/app/information/bloc/information_bloc.dart
+++ lib/src/ui/app/information/bloc/information_bloc.dart
@@ -1,4 +1,3 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:srigunting_app/src/domain/information.dart';
 import 'package:srigunting_app/src/domain/pagination.dart';
@@ -8,22 +7,16 @@ import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
 import 'package:srigunting_app/src/repository/information_repository.dart';
 import 'package:srigunting_app/src/repository/pagination.dart';
 import 'package:srigunting_app/src/repository/rest/tool/network_func.dart';
-
 part 'information_event.dart';
 part 'information_state.dart';
-
 class InformationBloc
     extends ABlocManagement<InformationEvent, InformationState> {
   final InformationRepository _informationRepository;
-
   InformationBloc(this._informationRepository) : super(InformationInitial()) {
     on<InformationInitialEvent>((event, emit) async {
       try {
         emit(InformationInitialLoading());
-
-        var res = await _informationRepository
-            .fetchInformation(PaginationRequest(page: 1, perPage: 10));
-
+        var res = await _informationRepository.fetchInformation(PaginationRequest(page: 1, perPage: 10));
         responseHandler<Pagination<Information>>(res, onSuccess: (response) {
           emit(InformationInitialLoaded(
             dataInformation: response?.data ?? [],
@@ -36,17 +29,13 @@ class InformationBloc
         emit(InformationInitialError(error: e.toString()));
       }
     });
-
     on<InformationLoadMoreEvent>((event, emit) async {
       try {
         emit(InformationLoadMoreLoading(
-          dataInformation: [],
+          dataInformation: const [],
           pagination: null,
         ));
-
-        var res = await _informationRepository
-            .fetchInformation(PaginationRequest(page: event.page, perPage: 10));
-
+        var res = await _informationRepository.fetchInformation(PaginationRequest(page: event.page, perPage: 10));
         responseHandler<Pagination<Information>>(res, onSuccess: (response) {
           emit(InformationLoadMoreLoaded(
             dataInformation: response?.data ?? [],
@@ -55,14 +44,14 @@ class InformationBloc
         }, onError: (dioError, code, errorMessage) {
           emit(InformationLoadMoreError(
             error: errorMessage,
-            dataInformation: [],
+            dataInformation: const [],
             pagination: null,
           ));
         });
       } catch (e) {
         emit(InformationLoadMoreError(
           error: e.toString(),
-          dataInformation: [],
+          dataInformation: const [],
           pagination: null,
         ));
       }
