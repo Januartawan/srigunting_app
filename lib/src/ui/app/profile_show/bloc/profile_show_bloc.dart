fd2ef3c8f12d69deeb19158d0e9816ba19e891ca
Januartawan <januarthawan@gmail.com>
2026-06-27 16:09:51 +0800

    Hapus import tidak terpakai, komentar, dan rapikan kode di profile_show_bloc.dart

--- lib/src/ui/app/profile_show/bloc/profile_show_bloc.dart
+++ lib/src/ui/app/profile_show/bloc/profile_show_bloc.dart
@@ -1,4 +1,3 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:srigunting_app/src/domain/guide.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/bloc/bloc_state.dart';
@@ -6,26 +5,18 @@ import 'package:srigunting_app/src/infrastructure/state_management/event.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
 import 'package:srigunting_app/src/repository/guide_repository.dart';
 import 'package:srigunting_app/src/repository/rest/tool/network_func.dart';
-
 part 'profile_show_event.dart';
 part 'profile_show_state.dart';
-
 class ProfileShowBloc
     extends ABlocManagement<ProfileShowEvent, ProfileShowState> {
   final GuideRepository _guideRepository;
-
   ProfileShowBloc(this._guideRepository) : super(ProfileShowInitial()) {
     on<ProfileShowInitialEvent>((event, emit) async {
       try {
         emit(ProfileShowLoading());
-
-        // Call both showGuide and detailAccountSrigunting
         var showGuideRes = await _guideRepository.showGuide();
         var detailAccountRes = await _guideRepository.detailAccountSrigunting();
-
-        // Handle showGuide response
         responseHandler<Guide>(showGuideRes, onSuccess: (showGuideData) {
-          // Handle detailAccountSrigunting response
           responseHandler<Guide>(detailAccountRes,
               onSuccess: (detailAccountData) {
             emit(ProfileShowLoaded(
