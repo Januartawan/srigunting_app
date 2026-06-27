ba1d1689ea46be175e087839c92c59791bead2b1
Januartawan <januarthawan@gmail.com>
2026-06-27 13:48:24 +0800

    Perbarui penamaan ApiUrl.freeVisitDetail dan hapus baris kosong di free_visit_detail_rest_repository.dart

--- lib/src/repository/rest/free_visit_detail_rest_repository.dart
+++ lib/src/repository/rest/free_visit_detail_rest_repository.dart
@@ -3,16 +3,13 @@ import 'package:srigunting_app/src/repository/api_url/api_url.dart';
 import 'package:srigunting_app/src/repository/api_url/base_remote.dart';
 import 'package:srigunting_app/src/repository/free_visit_detail_repository.dart';
 import 'package:srigunting_app/src/repository/rest/tool/result.dart';
-
 class FreeVisitDetailRestRepository extends BaseRemote
     implements FreeVisitDetailRepository {
   FreeVisitDetailRestRepository(super.dio);
-
   @override
   Future<Result<FreeVisit>> showFreeVisitDetail() async {
-    var result = await getMethod(ApiUrl.FREE_VISIT_DETAIL,
+    var result = await getMethod(ApiUrl.freeVisitDetail,
         converter: ((response) => FreeVisit.fromMap(response["data"])));
-
     return result;
   }
 }
