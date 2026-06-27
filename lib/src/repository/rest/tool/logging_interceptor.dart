0aff9d313f726aa90aff8387ff31c3171394526f
Januartawan <januarthawan@gmail.com>
2026-06-27 14:57:14 +0800

    Tambah pengecekan kDebugMode, optimasi JsonEncoder menjadi const, dan hapus import tidak terpakai di logging_interceptor.dart

--- lib/src/repository/rest/tool/logging_interceptor.dart
+++ lib/src/repository/rest/tool/logging_interceptor.dart
@@ -1,16 +1,18 @@
-import 'dart:async';
 import 'dart:convert';
 import 'package:dio/dio.dart';
-
+import 'package:flutter/foundation.dart';
 class LoggingInterceptor extends Interceptor {
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    print("Body Request");
+    if (kDebugMode) {
+      print("Body Request");
+    }
     if (options.data is! FormData) {
-      final prettyJson = JsonEncoder.withIndent("  ").convert(options.data);
-
-      print(prettyJson);
+      final prettyJson = const JsonEncoder.withIndent("  ").convert(options.data);
+      if (kDebugMode) {
+        print(prettyJson);
+      }
     }
     super.onRequest(options, handler);
   }
