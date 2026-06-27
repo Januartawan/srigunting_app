37650d8413f185c282c4a6d7add342d6d0c81fb7
Januartawan <januarthawan@gmail.com>
2026-06-27 16:00:18 +0800

    Hapus import yang tidak digunakan dan perapihan kode di free_ticket_use_bloc.dart

--- lib/src/ui/app/free_ticket_use/bloc/free_ticket_use_bloc.dart
+++ lib/src/ui/app/free_ticket_use/bloc/free_ticket_use_bloc.dart
@@ -1,4 +1,3 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:srigunting_app/src/domain/free_visit.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/bloc/bloc_state.dart';
@@ -7,21 +6,16 @@ import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
 import 'package:srigunting_app/src/repository/request/free_ticket_request.dart';
 import 'package:srigunting_app/src/repository/rest/tool/network_func.dart';
 import 'package:srigunting_app/src/repository/reward_repository.dart';
-
 part 'free_ticket_use_event.dart';
 part 'free_ticket_use_state.dart';
-
 class FreeTicketUseBloc
     extends ABlocManagement<FreeTicketUseEvent, FreeTicketUseState> {
   final RewardRepository _rewardRepository;
-
   FreeTicketUseBloc(this._rewardRepository) : super(FreeTicketUseInitial()) {
     on<FreeTicketUseSubmitEvent>((event, emit) async {
       try {
         emit(FreeTicketUseSubmitLoading());
-
         var res = await _rewardRepository.useFreeTicket(event.request);
-
         await responseHandler<FreeVisit>(res, onSuccess: (response) {
           emit(FreeTicketUseSubmitSuccess(freeVisit: response ?? FreeVisit()));
         }, onError: (dioError, code, errorMessage) {
