f79a578ba2da1e037b7349ea40f795b1b9c5bf8f
Januartawan <januarthawan@gmail.com>
2026-06-27 15:01:46 +0800

    Bersihkan kode, hapus komentar yang tidak digunakan, dan optimasi struktur widget di login_screen.dart

--- lib/src/ui/login/login_screen.dart
+++ lib/src/ui/login/login_screen.dart
@@ -8,41 +8,31 @@ import 'package:srigunting_app/src/infrastructure/state_management/ui.dart';
 import 'package:srigunting_app/src/infrastructure/theme/colors.dart';
 import 'package:srigunting_app/src/routing/routing_constant.dart';
 import 'package:srigunting_app/src/ui/login/bloc/login_bloc.dart';
-
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
-
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
-
 class _LoginScreenState
     extends AUIManagement<LoginBloc, LoginState, LoginScreen> {
   final _phoneC = TextEditingController();
   final _passwordC = TextEditingController();
   final _formKey = GlobalKey<FormState>();
-
   @override
   void onStart() {
     stateManagement.pushEvent(LoginInitialEvent());
     super.onStart();
   }
-
   @override
   Widget buildState(BuildContext context, LoginState state) {
     switch (state) {
       case LoginSuccess():
         pushNamedAndRemoveUntil(Routing.APP, (route) => false);
-      // pushReplacementNamed(Routing.APP);
-      // showToastSuccess(context, message: "Login Success");
       case LoginError():
         showToastError(context, message: state.error);
       case LoginInitial():
-      // TODO: Handle this case.
       case LoginLoading():
-      // TODO: Handle this case.
     }
-
     return SScaffold(
       headerBody: Container(
         padding: const EdgeInsets.all(24),
@@ -89,88 +79,58 @@ class _LoginScreenState
       ),
       body: Form(
         key: _formKey,
-        // autovalidateMode: AutovalidateMode.onUserInteraction,
-        child: Container(
-          child: Column(
-            children: [
-              STextField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your phone / username';
-                  }
-                  return null;
-                },
-                controller: _phoneC,
-                label: "No WhatsApp / Username",
-                // inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                // keyboardType: TextInputType.numberWithOptions(),
+        child: Column(
+          children: [
+            STextField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your phone / username';
+                }
+                return null;
+              },
+              controller: _phoneC,
+              label: "No WhatsApp / Username",
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            STextField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              controller: _passwordC,
+              label: "Password",
+              obscureText: true,
+            ),
+            const Spacer(),
+            SButton(
+              loading: state is LoginLoading,
+              textStyle: lightText.copyWith(
+                  fontSize: 14, fontWeight: FontWeight.w400),
+              suffixIcon: const Icon(
+                Icons.arrow_forward_rounded,
+                size: 14,
+                color: AppColors.textBrandOn,
               ),
-              const SizedBox(
-                height: 12,
-              ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.end,
-              //   children: [
-              //     GestureDetector(
-              //       onTap: () {
-              //         pushNamed(Routing.FORGOT_PASSWORD);
-              //       },
-              //       child: const Text(
-              //         'Forgot Password',
-              //         style: TextStyle(
-              //           fontSize: 14,
-              //           color: AppColors.bgBrandPrimary,
-              //           decoration: TextDecoration.underline,
-              //           decorationColor: AppColors.bgBrandPrimary,
-              //         ),
-              //       ),
-              //     ),
-              //   ],
-              // ),
-              // const SizedBox(
-              //   height: 6,
-              // ),
-              STextField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-                controller: _passwordC,
-                label: "Password",
-                obscureText: true,
-              ),
-              const Spacer(),
-              SButton(
-                loading: state is LoginLoading,
-                textStyle: lightText.copyWith(
-                    fontSize: 14, fontWeight: FontWeight.w400),
-                suffixIcon: const Icon(
-                  Icons.arrow_forward_rounded,
-                  size: 14,
-                  color: AppColors.textBrandOn,
-                ),
-                label: "Masuk",
-                buttonStyle: primaryStyleButton,
-                onPressed: () {
-                  // Navigator.popAndPushNamed(context, Routing.APP);
-                  if (_formKey.currentState?.validate() ?? false) {
-                    stateManagement.pushEvent(
-                      LoginExecuteEvent(
-                          username: _phoneC.text, password: _passwordC.text),
-                    );
-                  }
-                },
-              ),
-            ],
-          ),
+              label: "Masuk",
+              buttonStyle: primaryStyleButton,
+              onPressed: () {
+                if (_formKey.currentState?.validate() ?? false) {
+                  stateManagement.pushEvent(
+                    LoginExecuteEvent(
+                        username: _phoneC.text, password: _passwordC.text),
+                  );
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
   }
-
   @override
-  // TODO: implement initialData
   LoginState get initialData => LoginInitial();
 }
