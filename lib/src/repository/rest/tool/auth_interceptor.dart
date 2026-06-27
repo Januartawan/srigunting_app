31a718329c5e6d501f5ccf0201328cf40bfdaac7
Januartawan <januarthawan@gmail.com>
2026-06-27 13:58:24 +0800

    Refaktor LazyLoadScrollView, perbarui penamaan enum LoadingStatus, dan optimasi sintaksis kode di lasy_load_scroll_view.dart

--- lib/src/repository/rest/tool/auth_interceptor.dart
+++ lib/src/repository/rest/tool/auth_interceptor.dart
@@ -1,22 +1,16 @@
 import 'package:dio/dio.dart';
 import 'package:srigunting_app/src/infrastructure/services/local_storage/local_storage_repository.dart';
 import 'package:srigunting_app/src/repository/rest/header.dart';
-
 class AuthInterceptor extends Interceptor {
   final LocalStorageRepository _localStorageRepository;
-
   AuthInterceptor(this._localStorageRepository);
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
     var authID = await _localStorageRepository.read(LocalStorageKey.AUTH_TOKEN);
-    // var outletID =
-    //     await _localStorageRepository.read(LocalStorageKey.OUTLET_ID);
-    options.headers[Header.AUTHORIZATION] = authID;
-    // options.headers[Header.OUTLET] = outletID;
-    options.headers[Header.ACCEPT] = 'application/json';
-    options.headers[Header.CONTENT_TYPE] = 'application/json';
-
+    options.headers[Header.authorization] = authID;
+    options.headers[Header.accept] = 'application/json';
+    options.headers[Header.contentType] = 'application/json';
     super.onRequest(options, handler);
   }
 }
