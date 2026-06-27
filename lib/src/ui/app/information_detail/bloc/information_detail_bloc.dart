67d8b86ceb84b78d3e199be11ddd94c541bbd3bf
Januartawan <januarthawan@gmail.com>
2026-06-27 16:02:19 +0800

    Hapus import yang tidak digunakan dan rapikan kode di information_detail_bloc.dart

--- lib/src/ui/app/information_detail/bloc/information_detail_bloc.dart
+++ lib/src/ui/app/information_detail/bloc/information_detail_bloc.dart
@@ -1,4 +1,3 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:srigunting_app/src/domain/information.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/bloc/bloc_state.dart';
@@ -6,23 +5,17 @@ import 'package:srigunting_app/src/infrastructure/state_management/event.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
 import 'package:srigunting_app/src/repository/information_repository.dart';
 import 'package:srigunting_app/src/repository/rest/tool/network_func.dart';
-import 'package:srigunting_app/src/ui/app/information/bloc/information_bloc.dart';
-
 part 'information_detail_event.dart';
 part 'information_detail_state.dart';
-
 class InformationDetailBloc
     extends ABlocManagement<InformationDetailEvent, InformationDetailState> {
   final InformationRepository _informationRepository;
-
   InformationDetailBloc(this._informationRepository)
       : super(InformationDetailInitial()) {
     on<InformationDetailInitialEvent>((event, emit) async {
       try {
         emit(InformationDetailInitialLoading());
-
         var res = await _informationRepository.showInformation(event.slug);
-
         responseHandler<Information>(res, onSuccess: (response) {
           emit(InformationDetailInitialLoaded(
               dataInformation: response ?? Information()));
