b825948bbcd24e37fcf292035af37a934b23a3f4
Januartawan <januarthawan@gmail.com>
2026-06-27 13:45:50 +0800

    Perbarui konstanta ApiUrl dan optimasi kode di notification_rest_repository.dart

--- lib/src/repository/rest/notification_rest_repository.dart
+++ lib/src/repository/rest/notification_rest_repository.dart
@@ -8,20 +8,17 @@ import 'package:srigunting_app/src/repository/api_url/base_remote.dart';
 import 'package:srigunting_app/src/repository/notification_repository.dart';
 import 'package:srigunting_app/src/repository/pagination.dart';
 import 'package:srigunting_app/src/repository/rest/tool/result.dart';
-
 class NotificationRestRepository extends BaseRemote
     implements NotificationRepository {
   NotificationRestRepository(super.dio);
-
   @override
   Future<Result<Pagination<NotificationApp>>> fetchNotification(
       PaginationRequest request) async {
-    var url = ApiUrl.NOTIFICATION_LIST.queryParam(request.toJson());
+    var url = ApiUrl.notificationList.queryParam(request.toJson());
     var result = await getMethod(
       url,
       converter: ((response) {
         var meta = response['meta'] ?? response;
-        // Jika meta kosong (array atau null), set default meta
         if (meta == null || (meta is List && meta.isEmpty)) {
           meta = {
             "total": 0,
@@ -36,34 +33,27 @@ class NotificationRestRepository extends BaseRemote
         return Pagination<NotificationApp>.fromJson(meta, dataList);
       }),
     );
-
     return result;
   }
-
   @override
   Future<Result<NotificationApp>> showNotification(String slug) async {
-    var url = ApiUrl.NOTIFICATION_DETAIL.queryParam({'slug': slug});
+    var url = ApiUrl.notificationDetail.queryParam({'slug': slug});
     var result = await getMethod(url,
         converter: ((response) => NotificationApp.fromJson(response["data"])));
-
     return result;
   }
-
   @override
   Future<Result<FreeVisit>> showNotificationFreeVisit(String slug) async {
-    var url = ApiUrl.NOTIFICATION_DETAIL.queryParam({'slug': slug});
+    var url = ApiUrl.notificationDetail.queryParam({'slug': slug});
     var result = await getMethod(url,
         converter: ((response) => FreeVisit.fromMap(response["data"])));
-
     return result;
   }
-
   @override
   Future<Result<Transaction>> showNotificationTransaction(String slug) async {
-    var url = ApiUrl.NOTIFICATION_DETAIL.queryParam({'slug': slug});
+    var url = ApiUrl.notificationDetail.queryParam({'slug': slug});
     var result = await getMethod(url,
         converter: ((response) => Transaction.fromMap(response["data"])));
-
     return result;
   }
 }
