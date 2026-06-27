87b61b1e2b301ced98318703d3a85ffff2d79d26
Januartawan <januarthawan@gmail.com>
2026-06-27 14:50:18 +0800

    Ubah withOpacity menjadi withAlpha dan hapus baris kosong di SShimmerList

--- lib/src/infrastructure/components/atoms/shimmer/shimmer_list.dart
+++ lib/src/infrastructure/components/atoms/shimmer/shimmer_list.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
-
 class SShimmerList extends StatelessWidget {
   final double? height;
   final double? width;
-
   const SShimmerList({super.key, this.height, this.width});
-
   @override
   Widget build(BuildContext context) {
     return Shimmer.fromColors(
@@ -16,7 +13,7 @@ class SShimmerList extends StatelessWidget {
         width: width ?? 200,
         height: height ?? 20,
         decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.3),
+          color: Colors.white.withAlpha(76),
           borderRadius: BorderRadius.circular(6),
         ),
       ),
