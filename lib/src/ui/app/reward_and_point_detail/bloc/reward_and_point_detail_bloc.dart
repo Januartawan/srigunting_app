b31c82a66c303ffe279cee319834972460ff24f1
Januartawan <januarthawan@gmail.com>
2026-06-27 16:27:36 +0800

    Hapus import tidak terpakai dan bersihkan kode komentar di reward_and_point_detail_bloc.dart

--- lib/src/ui/app/reward_and_point_detail/bloc/reward_and_point_detail_bloc.dart
+++ lib/src/ui/app/reward_and_point_detail/bloc/reward_and_point_detail_bloc.dart
@@ -1,27 +1,17 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:srigunting_app/src/domain/transaction.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/bloc/bloc_state.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/event.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
-import 'package:srigunting_app/src/repository/rest/tool/network_func.dart';
-// import 'package:srigunting_app/src/repository/reward_and_point_detail_repository.dart';
-
 part 'reward_and_point_detail_event.dart';
 part 'reward_and_point_detail_state.dart';
-
 class RewardAndPointDetailBloc extends ABlocManagement<
     RewardAndPointDetailEvent, RewardAndPointDetailState> {
-  // final RewardAndPointDetailRepository _rewardAndPointDetailRepository;
-
   RewardAndPointDetailBloc() : super(RewardAndPointDetailInitial()) {
     //   on<RewardAndPointDetailInitialEvent>((event, emit) async {
     //     try {
     //       emit(RewardAndPointDetailLoading());
-
-    //       var res =
-    //           await _rewardAndPointDetailRepository.showRewardAndPointDetail();
-
+    //       var res = await _rewardAndPointDetailRepository.showRewardAndPointDetail();
     //       responseHandler<Transaction>(res, onSuccess: (response) {
     //         emit(RewardAndPointDetailLoaded(
     //             dataTransaction: response ?? Transaction()));
