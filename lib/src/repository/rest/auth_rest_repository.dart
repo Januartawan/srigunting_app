0cf17c5be6bbc274f957841b265a409d12a92bf8
Januartawan <januarthawan@gmail.com>
2026-06-27 13:52:05 +0800

    Perbarui penamaan konstanta ApiUrl ke camelCase dan rapikan format kode di auth_rest_repository.dart

--- lib/src/repository/rest/auth_rest_repository.dart
+++ lib/src/repository/rest/auth_rest_repository.dart
@@ -5,92 +5,74 @@ import 'package:srigunting_app/src/repository/api_url/base_remote.dart';
 import 'package:srigunting_app/src/repository/auth_repository.dart';
 import 'package:srigunting_app/src/repository/request/register_request.dart';
 import 'package:srigunting_app/src/repository/rest/tool/result.dart';
-
 class AuthRestRepository extends BaseRemote implements AuthRepository {
   AuthRestRepository(super.dio);
-
   @override
   Future<Result<LoginResponse>> login(
       {required String username, required String password}) async {
-    var url = ApiUrl.LOGIN;
-
+    var url = ApiUrl.login;
     var result = await postMethod(url,
         body: {"username": username, "password": password},
         converter: ((response) => LoginResponse.fromMap(response)));
-
     return result;
   }
-
   @override
   Future<Result> logout() async {
-    var result = await postMethod(ApiUrl.LOGOUT,
+    var result = await postMethod(ApiUrl.logout,
         body: {}, converter: (response) => response);
-
     return result;
   }
-
   @override
   Future<Result> register(RegisterRequest payload) async {
-    var result = await postMethod(ApiUrl.REGISTER,
+    var result = await postMethod(ApiUrl.register,
         body: payload.toMap(), converter: (response) => response);
-
     return result;
   }
-
   @override
   Future<Result> updateEmail(
       {required String email, required String password}) async {
-    var result = await patchMethod(ApiUrl.CHANGE_EMAIL,
+    var result = await patchMethod(ApiUrl.changeEmail,
         body: {"email": email, "password": password},
         converter: (response) => response);
-
     return result;
   }
-
   @override
   Future<Result> updatePassword(
       {required String newPassword, required String oldPassword}) async {
-    var result = await patchMethod(ApiUrl.CHANGE_PASSWORD,
+    var result = await patchMethod(ApiUrl.changePassword,
         body: {"password_baru": newPassword, "password_lama": oldPassword},
         converter: (response) => response);
-
     return result;
   }
-
   @override
   Future<Result> updateUsername(
       {required String username, required String password}) async {
-    var result = await patchMethod(ApiUrl.CHANGE_USERNAME,
+    var result = await patchMethod(ApiUrl.changeUsername,
         body: {"username": username, "password": password},
         converter: (response) => response);
-
     return result;
   }
-
   @override
   Future<void> loginSuccess() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isLoggedIn', true);
   }
-
   @override
   Future<void> logoutSuccess() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.remove('isLoggedIn');
   }
-
   @override
   Future<Result> updateDeviceToken({
     required String deviceToken,
     required String platform,
   }) async {
-    var result = await patchMethod(ApiUrl.DEVICE_TOKEN,
+    var result = await patchMethod(ApiUrl.deviceToken,
         body: {
           "device_token": deviceToken,
           "platform": platform,
         },
         converter: (response) => response);
-
     return result;
   }
 }
