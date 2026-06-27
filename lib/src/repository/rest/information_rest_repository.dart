b41d1e5dd36204b6bc108712d21d9276f004a929
Januartawan <januarthawan@gmail.com>
2026-06-27 13:46:52 +0800

    Perbarui penamaan konstanta ApiUrl menjadi camelCase dan pembersihan kode di information_rest_repository.dart

--- lib/src/repository/rest/information_rest_repository.dart
+++ lib/src/repository/rest/information_rest_repository.dart
@@ -7,20 +7,17 @@ import 'package:srigunting_app/src/repository/api_url/base_remote.dart';
 import 'package:srigunting_app/src/repository/information_repository.dart';
 import 'package:srigunting_app/src/repository/pagination.dart';
 import 'package:srigunting_app/src/repository/rest/tool/result.dart';
-
 class InformationRestRepository extends BaseRemote
     implements InformationRepository {
   InformationRestRepository(super.dio);
-
   @override
   Future<Result<Pagination<Information>>> fetchInformation(
       PaginationRequest request) async {
-    var url = ApiUrl.INFORMATION_LIST.queryParam(request.toJson());
+    var url = ApiUrl.informationList.queryParam(request.toJson());
     var result = await getMethod(
       url,
       converter: ((response) {
         var meta = response['meta'] ?? response;
-        // Jika meta kosong (array atau null), set default meta
         if (meta == null || (meta is List && meta.isEmpty)) {
           meta = {
             "total": 0,
@@ -35,21 +32,18 @@ class InformationRestRepository extends BaseRemote
         return Pagination<Information>.fromJson(meta, dataList);
       }),
     );
-
     return result;
   }
-
   @override
   Future<Result<Information>> showInformation(String slug) async {
-    var result = await getMethod('${ApiUrl.INFORMATION_DETAIL}/?slug=$slug',
+    var result = await getMethod('${ApiUrl.informationDetail}/?slug=$slug',
         converter: ((response) => Information.fromJson(response['data'])));
     return result;
   }
-
   @override
   Future<Result<List<String>>> fetchSkemaSusukImages() async {
     var result = await getMethod<List<String>>(
-      ApiUrl.SKEMA_SUSUK,
+      ApiUrl.skemaSusuk,
       headers: const {'Accept': 'application/json'},
       converter: ((response) {
         if (response is! Map || response['data'] is! List) {
@@ -57,17 +51,15 @@ class InformationRestRepository extends BaseRemote
               ? response['message'] ?? 'Data skema susuk tidak valid'
               : 'Data skema susuk tidak valid');
         }
-
         return List<String>.from(response['data']);
       }),
     );
     return result;
   }
-
   @override
   Future<Result<List<String>>> fetchFlowSusukImages() async {
     var result = await getMethod<List<String>>(
-      ApiUrl.FLOW_SUSUK,
+      ApiUrl.flowSusuk,
       headers: const {'Accept': 'application/json'},
       converter: ((response) {
         if (response is! Map || response['data'] is! List) {
@@ -75,17 +67,15 @@ class InformationRestRepository extends BaseRemote
               ? response['message'] ?? 'Data flow klaim tidak valid'
               : 'Data flow klaim tidak valid');
         }
-
         return List<String>.from(response['data']);
       }),
     );
     return result;
   }
-
   @override
   Future<Result<List<PricelistProgram>>> fetchPricelistProgram() async {
     var result = await getMethod<List<PricelistProgram>>(
-      ApiUrl.PRICELIST_PROGRAM,
+      ApiUrl.pricelistProgram,
       headers: const {'Accept': 'application/json'},
       converter: ((response) {
         if (response is! Map || response['data'] is! List) {
@@ -93,7 +83,6 @@ class InformationRestRepository extends BaseRemote
               ? response['message'] ?? 'Data pricelist program tidak valid'
               : 'Data pricelist program tidak valid');
         }
-
         return List<PricelistProgram>.from(
           response['data'].map((x) => PricelistProgram.fromJson(x)),
         );
@@ -101,11 +90,10 @@ class InformationRestRepository extends BaseRemote
     );
     return result;
   }
-
   @override
   Future<Result<List<String>>> fetchPricelistProgramDetail(
       String media) async {
-    var url = ApiUrl.PRICELIST_PROGRAM_DETAIL.queryParam({'media': media});
+    var url = ApiUrl.pricelistProgramDetail.queryParam({'media': media});
     var result = await getMethod<List<String>>(
       url,
       headers: const {'Accept': 'application/json'},
@@ -115,7 +103,6 @@ class InformationRestRepository extends BaseRemote
               ? response['message'] ?? 'Data detail tidak valid'
               : 'Data detail tidak valid');
         }
-
         return List<String>.from(response['data']);
       }),
     );
