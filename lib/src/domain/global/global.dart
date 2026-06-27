05b4fee610a1dbfc43c7b17e188e41eab235862c
Januartawan <januarthawan@gmail.com>
2026-06-27 12:50:48 +0800

    Hapus import material.dart dan optimasi format kode di global.dart

--- lib/src/domain/global/global.dart
+++ lib/src/domain/global/global.dart
@@ -1,21 +1,9 @@
 import 'dart:async';
-
-import 'package:flutter/material.dart';
-
-class Device {
-  final String? name, model, version;
-
-  Device({required this.name, this.model, this.version});
-
-  Map<String, dynamic> toJson() {
-    return {"name": name, "model": model, "version": version};
-  }
+class Device{
+  final String? name,model,version;
+  Device({required this.name,this.model,this.version});
+  Map<String,dynamic> toJson(){return {"name":name,"model":model,"version":version};}
 }
-
 Device? activeDevice;
-
-StreamController<bool> loadingStreamController = StreamController<bool>();
-StreamController<double> loadingProgressStreamController =
-    StreamController<double>();
-
-// var storage = SecureStorage();
+StreamController<bool> loadingStreamController=StreamController<bool>();
+StreamController<double> loadingProgressStreamController=StreamController<double>();
