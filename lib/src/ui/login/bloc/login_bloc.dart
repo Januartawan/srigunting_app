da5f4f574d43598db42d0cceb899cc2b9d87444e
Januartawan <januarthawan@gmail.com>
2026-06-27 15:53:11 +0800

    Optimasi kode LoginBloc, hapus import bloc.dart, dan bersihkan kode komentar

--- lib/src/ui/login/bloc/login_bloc.dart
+++ lib/src/ui/login/bloc/login_bloc.dart
@@ -1,4 +1,3 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:srigunting_app/src/domain/login_response.dart';
 import 'package:srigunting_app/src/infrastructure/services/local_storage/local_storage_repository.dart';
@@ -7,32 +6,22 @@ import 'package:srigunting_app/src/infrastructure/state_management/event.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
 import 'package:srigunting_app/src/repository/auth_repository.dart';
 import 'package:srigunting_app/src/repository/rest/tool/network_func.dart';
-
 part 'login_event.dart';
 part 'login_state.dart';
-
 class LoginBloc extends ABlocManagement<LoginEvent, LoginState> {
   final AuthRepository _authRepository;
   final LocalStorageRepository _localStorageRepository;
-
   LoginBloc(this._authRepository, this._localStorageRepository)
       : super(LoginInitial()) {
     on<LoginInitialEvent>((event, emit) async {});
-
     on<LoginExecuteEvent>((event, emit) async {
       try {
         emit(LoginLoading());
-
         var res = await _authRepository.login(
             username: event.username, password: event.password);
-
         await responseHandler<LoginResponse>(res, onSuccess: (data) {
           _localStorageRepository.write(
-              LocalStorageKey.AUTH_TOKEN, 'Bearer ${data?.data?.token}' ?? '');
-          // ..write(LocalStorageKey.IS_LOGGED_IN, 'true');
-
-          // _authRepository.loginSuccess();x
-
+              LocalStorageKey.AUTH_TOKEN, 'Bearer ${data?.data?.token}');
           emit(LoginSuccess());
         }, onError: (dioError, code, errorMessage) {
           emit(LoginError(error: errorMessage));
