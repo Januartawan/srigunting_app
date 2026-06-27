f39b42c7073fa44f53c485948950d68184896131
Januartawan <januarthawan@gmail.com>
2026-06-27 13:52:33 +0800

    Refaktor penamaan konstanta menjadi camelCase di api_url.dart

--- lib/src/repository/api_url/api_url.dart
+++ lib/src/repository/api_url/api_url.dart
@@ -1,52 +1,52 @@
 class ApiUrl {
-  static const LOGIN = "/api/login";
-  static const LOGOUT = "/api/logout";
-  static const REGISTER = "/api/register";
-  static const CHANGE_USERNAME = "/api/change-username";
-  static const CHANGE_PASSWORD = "/api/change-password";
-  static const CHANGE_EMAIL = "/api/change-email";
+  static const login = "/api/login";
+  static const logout = "/api/logout";
+  static const register = "/api/register";
+  static const changeUsername = "/api/change-username";
+  static const changePassword = "/api/change-password";
+  static const changeEmail = "/api/change-email";
 
-  static const DETAIL_GUIDE = "/api/guide-detail";
-  static const GUIDE_UPDATE = "/api/guide-update";
-  static const GUIDE_AKUN_APPS = "/api/guide-akun-apps";
+  static const detailGuide = "/api/guide-detail";
+  static const guideUpdate = "/api/guide-update";
+  static const guideAkunApps = "/api/guide-akun-apps";
 
-  static const BALANCE = "/api/balance";
-  static const SCAN_TICKET = "/api/scan-ticket";
-  static const CLAIM_TICKET = "/api/claim-ticket";
-  static const SUSUK_HISTORY = "/api/susuk-history";
-  static const SUSUK_DETAIL = "/api/susuk-detail";
+  static const balance = "/api/balance";
+  static const scanTicket = "/api/scan-ticket";
+  static const claimTicket = "/api/claim-ticket";
+  static const susukHistory = "/api/susuk-history";
+  static const susukDetail = "/api/susuk-detail";
 
-  static const POINT_DETAIL = "/api/point-detail";
-  static const POINT_REWARD = "/api/point-reward";
-  static const POINT_UNDIAN = "/api/point-undian";
-  static const POINT_HISTORY = "/api/point-history";
-  static const REWARD_LIST = "/api/reward-list";
-  static const REWARD_DETAIL = "/api/reward-detail";
-  static const FREE_VISIT = "/api/free-visit";
-  static const FREE_VISIT_CLAIM = "/api/claim-free-visit";
-  static const FREE_VISIT_DETAIL = "/api/free-visit-detail";
+  static const pointDetail = "/api/point-detail";
+  static const pointReward = "/api/point-reward";
+  static const pointUndian = "/api/point-undian";
+  static const pointHistory = "/api/point-history";
+  static const rewardList = "/api/reward-list";
+  static const rewardDetail = "/api/reward-detail";
+  static const freeVisit = "/api/free-visit";
+  static const freeVisitClaim = "/api/claim-free-visit";
+  static const freeVisitDetail = "/api/free-visit-detail";
 
-  static const EVENT_LIST = "/api/event-list";
-  static const EVENT_DETAIL = "/api/event-detail";
+  static const eventList = "/api/event-list";
+  static const eventDetail = "/api/event-detail";
 
-  static const CITY = "/api/kab-kota";
-  static const GENDER = "/api/jenis-kelamin";
-  static const RELIGION = "/api/agama";
-  static const MARRIAGE_STATUS = "/api/status-perkawinan";
-  static const BASE_AREA = "/api/area-pangkalan";
+  static const city = "/api/kab-kota";
+  static const gender = "/api/jenis-kelamin";
+  static const religion = "/api/agama";
+  static const marriageStatus = "/api/status-perkawinan";
+  static const baseArea = "/api/area-pangkalan";
 
-  static const INFORMATION_LIST = "/api/information-list";
-  static const INFORMATION_DETAIL = "/api/information-detail";
-  static const SKEMA_SUSUK = "/api/skema-susuk";
-  static const FLOW_SUSUK = "/api/flow-susuk";
-  static const PRICELIST_PROGRAM = "/api/pricelist-program";
-  static const PRICELIST_PROGRAM_DETAIL = "/api/pricelist-program-detail";
+  static const informationList = "/api/information-list";
+  static const informationDetail = "/api/information-detail";
+  static const skemaSusuk = "/api/skema-susuk";
+  static const flowSusuk = "/api/flow-susuk";
+  static const pricelistProgram = "/api/pricelist-program";
+  static const pricelistProgramDetail = "/api/pricelist-program-detail";
 
-  static const FEEDBACK = "/api/feedback";
-  static const FEEDBACK_STORE = "/api/feedback-store";
+  static const feedback = "/api/feedback";
+  static const feedbackStore = "/api/feedback-store";
 
-  static const NOTIFICATION_LIST = "/api/notification-list";
-  static const NOTIFICATION_DETAIL = "/api/notification-detail";
+  static const notificationList = "/api/notification-list";
+  static const notificationDetail = "/api/notification-detail";
 
-  static const DEVICE_TOKEN = "/api/device-token";
+  static const deviceToken = "/api/device-token";
 }
