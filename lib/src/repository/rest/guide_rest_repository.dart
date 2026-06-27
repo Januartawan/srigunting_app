916f39ac8f86476919cd5c0704fc3ad7a85bbd4b
Januartawan <januarthawan@gmail.com>
2026-06-27 13:47:36 +0800

    Perbarui penggunaan konstanta ApiUrl dan optimasi format kode di guide_rest_repository.dart

--- lib/src/repository/rest/guide_rest_repository.dart
+++ lib/src/repository/rest/guide_rest_repository.dart
@@ -4,32 +4,25 @@ import 'package:srigunting_app/src/repository/api_url/base_remote.dart';
 import 'package:srigunting_app/src/repository/guide_repository.dart';
 import 'package:srigunting_app/src/repository/request/register_request.dart';
 import 'package:srigunting_app/src/repository/rest/tool/result.dart';
-
 class GuideRestRepository extends BaseRemote implements GuideRepository {
   GuideRestRepository(super.dio);
-
   @override
   Future<Result<Guide>> detailAccountSrigunting() async {
-    var result = await getMethod(ApiUrl.GUIDE_AKUN_APPS,
+    var result = await getMethod(ApiUrl.guideAkunApps,
         converter: ((response) => Guide.fromMap(response["data"])));
-
     return result;
   }
-
   @override
   Future<Result<Guide>> showGuide() async {
-    var result = await getMethod<Guide>(ApiUrl.DETAIL_GUIDE,
+    var result = await getMethod<Guide>(ApiUrl.detailGuide,
         converter: ((response) => Guide.fromMap(response["data"])));
-
     return result;
   }
-
   @override
   Future<Result> updateGuide(RegisterRequest payload) async {
     var p = payload.toMap();
-    var result = await patchMethod(ApiUrl.GUIDE_UPDATE,
+    var result = await patchMethod(ApiUrl.guideUpdate,
         body: p, converter: ((response) => response));
-
     return result;
   }
 }
