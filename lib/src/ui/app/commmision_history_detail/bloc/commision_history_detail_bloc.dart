0c85a9f31c0b013fbfdac4b9ef6b9196bee63a61
Januartawan <januarthawan@gmail.com>
2026-06-27 15:56:33 +0800

    Hapus import tidak terpakai dan rapikan format kode di commision_history_detail_bloc.dart

--- lib/src/ui/app/commmision_history_detail/bloc/commision_history_detail_bloc.dart
+++ lib/src/ui/app/commmision_history_detail/bloc/commision_history_detail_bloc.dart
@@ -1,4 +1,3 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:srigunting_app/src/domain/transaction.dart';
 import 'package:srigunting_app/src/domain/transaction_detail.dart';
@@ -7,23 +6,17 @@ import 'package:srigunting_app/src/infrastructure/state_management/event.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
 import 'package:srigunting_app/src/repository/balance_repopsitory.dart';
 import 'package:srigunting_app/src/repository/rest/tool/network_func.dart';
-
 part 'commision_history_detail_event.dart';
 part 'commision_history_detail_state.dart';
-
 class CommisionHistoryDetailBloc extends ABlocManagement<
     CommisionHistoryDetailEvent, CommisionHistoryDetailState> {
   final BalanceRepopsitory _balanceRepopsitory;
-
   CommisionHistoryDetailBloc(this._balanceRepopsitory)
       : super(CommisionHistoryDetailInitial()) {
     on<CommisionHistoryDetailInitialEvent>((event, emit) async {
       try {
         emit(CommisionHistoryDetailLoading());
-
-        var res =
-            await _balanceRepopsitory.showTransactionDetail(event.trxCode);
-
+        var res = await _balanceRepopsitory.showTransactionDetail(event.trxCode);
         await responseHandler<TransactionDetail>(res, onSuccess: (response) {
           emit(CommisionHistoryDetailLoaded(
             transaction: event.transaction,
