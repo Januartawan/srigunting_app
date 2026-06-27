9abeee858b6432cba0155d512c49c473449d79c2
Januartawan <januarthawan@gmail.com>
2026-06-27 16:12:47 +0800

    Hapus import bloc.dart dan optimasi baris kode di profile_update_username_bloc.dart

--- lib/src/ui/app/profile_update_username/bloc/profile_update_username_bloc.dart
+++ lib/src/ui/app/profile_update_username/bloc/profile_update_username_bloc.dart
@@ -1,18 +1,14 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/bloc/bloc_state.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/event.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
 import 'package:srigunting_app/src/repository/auth_repository.dart';
 import 'package:srigunting_app/src/repository/rest/tool/network_func.dart';
-
 part 'profile_update_username_event.dart';
 part 'profile_update_username_state.dart';
-
 class ProfileUpdateUsernameBloc extends ABlocManagement<
     ProfileUpdateUsernameEvent, ProfileUpdateUsernameState> {
   final AuthRepository _authRepository;
-
   ProfileUpdateUsernameBloc(this._authRepository)
       : super(ProfileUpdateUsernameInitial()) {
     on<ProfileUpdateUsernameExecuteEvent>((event, emit) async {
