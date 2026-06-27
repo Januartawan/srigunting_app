e2e9871a1068f324414ab478d7ca32d4db389711
Januartawan <januarthawan@gmail.com>
2026-06-27 12:46:10 +0800

    Ubah nama method emit menjadi emitState di management.dart

--- lib/src/infrastructure/state_management/management.dart
+++ lib/src/infrastructure/state_management/management.dart
@@ -2,7 +2,7 @@ import 'package:srigunting_app/src/infrastructure/state_management/event.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
 
 abstract class AStateManagement<AE extends AStateEvent, AS extends AState> {
-  void emit(AS state);
+  void emitState(AS state);
   Stream<AS> get stateStream;
   T? getStates<T extends AS>();
 
