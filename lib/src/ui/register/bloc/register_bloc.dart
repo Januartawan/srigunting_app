b17a388f47834f0080f2a2346faee1fbb7cb3352
Januartawan <januarthawan@gmail.com>
2026-06-27 16:17:35 +0800

    Hapus import bloc.dart dan bersihkan kode di register_bloc.dart

--- lib/src/ui/register/bloc/register_bloc.dart
+++ lib/src/ui/register/bloc/register_bloc.dart
@@ -1,4 +1,3 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/bloc/bloc_state.dart';
 import 'package:srigunting_app/src/infrastructure/state_management/event.dart';
@@ -6,20 +5,15 @@ import 'package:srigunting_app/src/infrastructure/state_management/state.dart';
 import 'package:srigunting_app/src/repository/auth_repository.dart';
 import 'package:srigunting_app/src/repository/request/register_request.dart';
 import 'package:srigunting_app/src/repository/rest/tool/network_func.dart';
-
 part 'register_event.dart';
 part 'register_state.dart';
-
 class RegisterBloc extends ABlocManagement<RegisterEvent, RegisterState> {
   final AuthRepository _authRepository;
-
   RegisterBloc(this._authRepository) : super(RegisterInitial()) {
     on<RegisterExecute>((event, emit) async {
       try {
         emit(RegisterExecuteLoading());
-
         var res = await _authRepository.register(event.payload);
-
         responseHandler(res, onSuccess: (response) {
           emit(RegisterExecuteSuccess());
         }, onError: (dioError, code, errorMessage) {
